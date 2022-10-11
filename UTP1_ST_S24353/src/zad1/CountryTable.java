e33f7625e2e75d8fe4dda58611c7f67a160eae38
Tomasz Serafiński <tomek.serafinski@gmail.com>
2022-10-11 20:01:33 +0200

    working base

--- UTP1_ST_S24353/src/zad1/CountryTable.java
+++ UTP1_ST_S24353/src/zad1/CountryTable.java
@@ -1,41 +1,41 @@
 package zad1;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Vector;
 
 public class CountryTable {
 
+    JTable table = new JTable();
+
+    DefaultTableModel model = new DefaultTableModel();
     String linia;
     String[] naglowki;
-
-    Vector<String[]> dane = new Vector<String[]>();
     String[] tmp;
+
     public CountryTable(String countriesFileName) throws IOException {
         FileReader fileReader = new FileReader(countriesFileName);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
+
         //uzyskanie nagłówków
         linia = bufferedReader.readLine();
         naglowki = linia.split("\t");
+        model.setColumnIdentifiers(naglowki);
         linia = bufferedReader.readLine();
 
-        //
+        //Reszta pliku
         while (linia != null){
             tmp = linia.split("\t");
-            dane.add(tmp);
-            System.out.println(Arrays.toString(tmp));
+            model.addRow(tmp);
             linia = bufferedReader.readLine();
         }
 
     }
 
     public JTable create() {
-        System.out.println();
-        String [][] data = {{"Republic of Poland", "Warsaw", "38500"},{"Chech Republic", "Prague", "10500"}};
-        String []column = naglowki;
-        return new JTable(data,column);
+        table.setModel(model);
+        return table;
     }
 }
